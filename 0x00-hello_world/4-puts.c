88d818cbbe2279902614f343cf57577b9f2e29a8
lawal kazeem <quazeemastic@gmail.com>
2022-09-08 13:37:39 -0700

    C is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Ths programs is usin puts indtead of printf as out put
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n") ;
+	return(0) ;
+}	
+
