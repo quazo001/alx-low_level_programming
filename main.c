e4ec2ec95ee84001fb0fd6c5be846ee77d18e6c9
lawal kazeem <quazeemastic@gmail.com>
2022-09-08 17:27:30 +0100

    C programming is fun

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main- A script that runs a C file through the preprocessor and save the result into another file
+  * Return: Always (Success)
+*/
+
+int main(void) 
+{
+	 printf("Hello, world");
+	 return (0);
+}
