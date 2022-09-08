67566f2601eb2ac5705b37d93893222780505807
lawal kazeem <quazeemastic@gmail.com>
2022-09-08 14:22:58 -0700

    C programming is cool

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- main entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+init main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
