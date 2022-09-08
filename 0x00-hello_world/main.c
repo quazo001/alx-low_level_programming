53ef2b187eb79d747fec627ad900daf89a9b5855
lawal kazeem <quazeemastic@gmail.com>
2022-09-08 11:33:18 -0700

    C programmin is fun

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main-Fist c programming deployement hello world preprocessor
+ * return: Always success
+ */
+int main(void)
+{
+	printf("hello word this is my first program in c") ;
+	return(0) ;
+}
