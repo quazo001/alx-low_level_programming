be8f775dabfee3b05ca7dfc1db3a09019d50615e
lawal kazeem <quazeemastic@gmail.com>
2022-09-13 15:31:32 -0700

    Alx low level programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - A function that prints _putchar with a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char f[] = "_putchar";
+
+	int p;
+
+	for (p = 0; p < 5; p++)
+	{
+		_putchar(f[p]);
+	}
+	_putchar('n\');
+	return (0)
+}
