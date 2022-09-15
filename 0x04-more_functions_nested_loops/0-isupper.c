8981da1d30e745149deedad8b563b426ff8d1cac
lawal kazeem <quazeemastic@gmail.com>
2022-09-15 14:45:36 -0700

    C programming is not an easy

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * Return: Always 0 (success)
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
