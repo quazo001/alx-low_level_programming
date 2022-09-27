9c249fe5bd73b68c61c541d26991ccb7efde83ed
quazo001 <49644714+quazo001@users.noreply.github.com>
2022-09-27 02:11:45 +0100

    Task-0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-#include <main.h>
-
-/**
- * -memset - fills mempry with a constant byte,
- * @s: memory area.
- * @b: constant byte.
- * @n: bytes filled.
- * Return: the pointer to dets.
- */
-
-car *_memset(char _s, char b, unsigned int n)
-=======
 #include "main.h"
 
 /**
@@ -22,16 +9,11 @@ car *_memset(char _s, char b, unsigned int n)
 */
 
 char *_memset(char *s, char b, unsigned int n)
->>>>>>> f002eaae3c877af17eaf53256ec407198bb92d9f
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-<<<<<<< HEAD
-		*(s + i) = b;
-=======
 		*(s + i) =  b;
->>>>>>> f002eaae3c877af17eaf53256ec407198bb92d9f
 
 	return (s);
 }
